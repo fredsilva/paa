ae4ae99e1116469db0b8d75c461ff7b8b86c2f56
fredsilva <fredsilva.sistemas@gmail.com>
2014-02-12 00:16:07 -0300

    Sem o calculo de tempo de execução

--- forca_bruta.py
+++ forca_bruta.py
@@ -5,23 +5,17 @@ UFT - Universidade Federal do Tocantins
 '''
 
 from random import randint, shuffle, choice
-from time import time
 import string
 
 def bubble_sort(lista):
 	'''
 	Realiza a ordenação de uma lista com o método Bubble Sort
-	'''	
-	inicio = time()
+	'''		
 
 	for i in range(0, len(lista)-1):
 		for j in range(0, len(lista)-1-i):
 			if lista[j] > lista[j+1]:
-				lista[j], lista[j+1] = lista[j+1], lista[j]		
-
-	tempo_exe = time() - inicio
-	print('Tempo de execução: %f s' % tempo_exe)
-
+				lista[j], lista[j+1] = lista[j+1], lista[j]	
 	return lista
 
 
@@ -29,18 +23,12 @@ def selection_sort(lista):
 	'''
 	Realiza a ordenação de uma lista com o método Selection Sort
 	'''
-	inicio = time()
-
 	for i in range(0, len(lista)-1):
 		menor = i
 		for j in range(i + 1, len(lista)):
 			if lista[j] < lista[menor]:
 				menor = j
 		lista[i], lista[menor] = lista[menor], lista[i]
-
-	tempo_exe = time() - inicio
-	print('Tempo de execução: %f s' % tempo_exe)
-
 	return lista
 
 
@@ -48,39 +36,19 @@ def busca_sequencial(item, lista):
 	'''
 	Realiza uma busca sequencial em uma lista
 	'''
-	inicio = time()
-	
 	for i, v in enumerate(lista):
-		if item == v:
-			
-			tempo_exe = time() - inicio			
-			print('Tempo de execução: %f s' % tempo_exe)
-
+		if item == v:		
 			return i	
-	
-	tempo_exe = time() - inicio
-	print('Tempo de execução: %f s' % tempo_exe)
-
 	return False
 
 def compara_strings(texto, amostra):
 	'''
 	Comparação de strings por força bruta
-	'''
-	inicio = time()
-	
+	'''		
 	for i, caracter in enumerate(amostra):
 		if texto[0] == caracter:			
 			if sub_string(texto, amostra[i:len(texto)]):
-				
-				tempo_exe = time() - inicio
-				print('Tempo de execução: %f s' % tempo_exe)
-				
 				return True
-	
-	tempo_exe = time() - inicio
-	print('Tempo de execução: %f s' % tempo_exe)				
-	
 	return False			
 
 def sub_string(texto, sub_amostra):
@@ -97,8 +65,7 @@ def sub_string(texto, sub_amostra):
 def par_proximo(lista):
 	'''
 	Retorna os pares mais próximos dado um conjunto de pontos
-	'''
-	inicio = time()
+	'''	
 	dmin = 1000000 
 	for i in range(0, len(lista)-1):		
 		for j in range(i + 1, len(lista)):
@@ -106,9 +73,7 @@ def par_proximo(lista):
 		if dif < dmin:
 			dmin = dif
 			index1 = i;
-			index2 = j;	
-	tempo_exe = time() - inicio
-	print('Tempo de execução: %f s' % tempo_exe)
+			index2 = j;		
 	return (index1,index2)
 
 
